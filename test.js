329cf8354caac0762108c9a371210b6cbaca8b7d
Teruk0 <108195723+Teruk0@users.noreply.github.com>
2023-10-04 13:57:31 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+const { createWorker } = require('tesseract.js');
+
+const worker = await createWorker('eng', 1, {
+  logger: m => console.log(m), // Add logger here
+});
+
+(async () => {
+  const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  console.log(text);
+  await worker.terminate();
+})();
